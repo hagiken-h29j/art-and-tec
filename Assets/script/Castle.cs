3667a87f0795cfa79a9e1c1a069b14945598b5ee
nakamura-umi <40160565+nakamura-umi@users.noreply.github.com>
2020-04-12 14:19:10 +0900

    自陣の用意
    自陣を用意した。

---
+++ Assets/script/Castle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour
+{
+    //自陣。敵接触時とかの挙動。
+    //今は敵接触消去。継続HP減少とかもあり
+    //依存→なし
+    //Resources→なし
+    //Tag→Enemy
+
+    public int HP = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        // 物体がトリガーに接触しとき、今は敵消去・HP減少
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            HP--;
+            if(HP <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
